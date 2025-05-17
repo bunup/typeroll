caf37b3fb39a81cd9d9887db7dfb35d8c3e92af6
arshad <arshadpyaseen@gmail.com>
2025-05-17 16:26:26 +0530

    chore: update

--- test/generate.ts
+++ test/generate.ts
@@ -1,8 +1,24 @@
-import { generateJsonSchema } from "../jc";
-import path from "node:path";
+import { dts } from "../src";
 
-const sourceFile = path.join(__dirname, "../src/types.ts");
-const sourceContent = await Bun.file(sourceFile).text();
-const typeName = "GenerateDtsOptions";
-
-console.log(generateJsonSchema(sourceFile, sourceContent, typeName));
+await Bun.build({
+    entrypoints: ["project/index.ts"],
+    plugins: [
+        dts({
+            resolve: true,
+        }),
+    ],
+    target: "node",
+    external: [
+        "oxc-transform",
+        "oxc-resolver",
+        "typescript",
+        "ts-import-resolver",
+        "oxc-parser",
+        "bun-dts",
+        "elysia",
+        "bun",
+        "publint",
+        "unplugin-unused",
+    ],
+    outdir: "test/dist",
+});
