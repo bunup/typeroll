cdb93cc1e6bf7f4e065829394a55be9a9722f504
arshad <arshadpyaseen@gmail.com>
2025-06-01 18:26:25 +0530

    chore: improve name conflict error message

--- src/logger.ts
+++ src/logger.ts
@@ -39,17 +39,20 @@ export function handleBunBuildLogs(
 	for (const log of logs) {
 		if (log.level === 'error') {
 			if (log.message.includes('Multiple exports with the same name')) {
-				logger.info(
-					`Found multiple exports with the same name in ${pc.underline(log.position?.file)}.
+				console.log()
+				logger.error(
+					`Found multiple exports with the same name in ${pc.underline(log.position?.file)}.`,
+				)
 
-You cannot have a type export and a value export with the same name in the same file.
+				console.log(
+					`\nYou cannot have a type export and a value export with the same name in the same file.
 
 For example, this is not allowed:
 
 export const ${log.position?.lineText.split(' ')[2]} = ...
 export type ${log.position?.lineText.split(' ')[2]} = ...
 
-Consider renaming the type export to a different name or moving the type export to a separate file (e.g., types.ts) to resolve this naming conflict.`,
+Consider renaming the type export to a different name or moving the type export to a separate file (e.g., types.ts) to resolve this naming conflict.\n`,
 				)
 
 				throw new Error(
